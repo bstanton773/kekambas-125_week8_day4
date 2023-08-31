4222010ff019bab6f6fd90088e3d1dbbfab12256
Brian <bstanton773@gmail.com>
2023-08-31 14:24:23 -0500

    Add shape containers for the Component

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import { Canvas } from './Widget';
+import { Canvas, Component } from './Widget';
 
 
 const canvas = new Canvas(document.body);
 console.log(canvas);
+
+const myComponent = new Component();
+console.log(myComponent);
+console.log(myComponent.shape);
+console.log(myComponent.shape.attributes);
